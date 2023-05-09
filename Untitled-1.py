71d229253fdc8ff77911ec19c2a10391fa008e7f
KhuatDucThinh <129965297+KhuatDucThinh@users.noreply.github.com>
2023-05-10 00:11:57 +0700

    Hey

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+from queue import *
+if __name__ == '__main__':
+    q= Queue(-1)
+
+    print('----- Put -----')
+    for x in range(1,6):
+        print(f'Put {x}')
+        q.put(x)
+
+    print('----- Get -----')
+    while not q.empty():
+        gt = q.get()
+        print(f'Get ->{gt}')
---
+++ py1.py
@@ -0,0 +1,33 @@
+import random 
+def san_sinh_mang(n):
+    mang =[]
+    for i in range(n):
+        so_ngau_nhien = random.randint(-100, 100)
+        mang.append(so_ngau_nhien)
+
+    return mang
+def tim_tuyen_tinh(mang, x):
+    n = len(mang)
+    for i in range(n):
+        if mang[i] == x:
+            return i
+        
+
+    return -1
+
+def main():
+    mang = san_sinh_mang(20)
+    print(mang)
+
+    x= int(input("nhap vao gia tri can tim:"))
+    vitri = tim_tuyen_tinh(mang, x)
+
+    if vitri != -1:
+        print(f"phan tu {x} duoc tim thay tai vi tri {vitri}")
+    else:
+        print(f"Khong tim thay gia tri {x} trong mang")
+
+if __name__ =="__main__":
+    main()
+
+
